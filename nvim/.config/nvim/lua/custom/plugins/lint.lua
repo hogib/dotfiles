7826591303eceb11db41607076a042bf417932cf
oguzb <hoguzbolat@gmail.com>
2026-06-14 23:38:21 +0300

    clippy

--- nvim/.config/nvim/lua/custom/plugins/lint.lua
+++ nvim/.config/nvim/lua/custom/plugins/lint.lua
@@ -11,6 +11,7 @@ return {
       markdown = { 'markdownlint' },
       c = { 'cppcheck' },
       cpp = { 'cppcheck' },
+      rst = { 'clippy' },
     }
 
     -- To allow other plugins to add linters to require('lint').linters_by_ft,
