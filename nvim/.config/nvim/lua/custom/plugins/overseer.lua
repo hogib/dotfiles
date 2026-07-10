ee3712ca6acb985f3716dcade609c2406761d224
OguzB <hoguzbolat@gmail.com>
2026-07-10 22:36:39 +0300

    magic added

--- nvim/.config/nvim/lua/custom/plugins/overseer.lua
+++ nvim/.config/nvim/lua/custom/plugins/overseer.lua
@@ -1,37 +1,38 @@
 return {
   'stevearc/overseer.nvim',
-  config = function()
-    require('overseer').setup {
-      -- Strategy used for running tasks
-      strategy = 'terminal',
+  dependencies = { 'stevearc/dressing.nvim' },
 
-      -- Template modules to load
-      templates = { 'builtin', 'user' },
+  cmd = {
+    'OverseerRun',
+    'OverseerOpen',
+    'OverseerInfo',
+    'OverseerToggle',
+    'OverseerShell',
+    'OverseerTaskAction',
+  },
 
-      -- Configure the task list UI
-      task_list = {
-        direction = 'bottom',
-        min_height = 10,
-        max_height = 20,
-        default_detail = 1,
+  keys = {
+    { '<leader>rr', '<cmd>OverseerRun<CR>', desc = 'Run task' },
+    { '<leader>rt', '<cmd>OverseerToggle<CR>', desc = 'Toggle task list' },
+    { '<leader>rc', '<cmd>OverseerShell<CR>', desc = 'Run shell command' },
+    { '<leader>ra', '<cmd>OverseerTaskAction<CR>', desc = 'Task action' },
+  },
+
+  opts = {
+    strategy = 'terminal',
+    templates = { 'builtin', 'user.mesonbuild' },
+    task_list = {
+      direction = 'bottom',
+      min_height = 10,
+      max_height = 20,
+      default_detail = 1,
+    },
+    component_aliases = {
+      default = {
+        'on_exit_set_status',
+        'on_complete_notify',
+        { 'on_complete_dispose', require_view = { 'SUCCESS', 'FAILURE' } },
       },
-
-      -- Aliases for component lists
-      component_aliases = {
-        -- Most tasks are initialized with the default components
-        default = {
-          'on_exit_set_status',
-          'on_complete_notify',
-          { 'on_complete_dispose', require_view = { 'SUCCESS', 'FAILURE' } },
-        },
-      },
-    }
-
-    -- Set up useful keymaps
-    vim.keymap.set('n', '<leader>rr', '<cmd>OverseerRun<CR>', { desc = 'Run task' })
-    vim.keymap.set('n', '<leader>rt', '<cmd>OverseerToggle<CR>', { desc = 'Toggle task list' })
-    vim.keymap.set('n', '<leader>rc', '<cmd>OverseerRunCmd<CR>', { desc = 'Run shell command' })
-    vim.keymap.set('n', '<leader>ra', '<cmd>OverseerTaskAction<CR>', { desc = 'Task action' })
-    vim.keymap.set('n', '<leader>ri', '<cmd>OverseerInfo<CR>', { desc = 'Overseer Info' })
-  end,
+    },
+  },
 }
--- nvim/.config/nvim/lua/overseer/template/user/mesonbuild.lua
+++ nvim/.config/nvim/lua/overseer/template/user/mesonbuild.lua
@@ -1,39 +1,50 @@
-local is_meson_project = {
-  callback = function(search) return vim.fn.filereadable 'meson.build' == 1 end,
-}
-
 return {
-  {
-    name = 'Meson Compile',
-    builder = function()
-      return {
-        cmd = { 'meson' },
-        args = { 'compile', '-C', 'builddir' },
-        components = { 'default', { 'on_output_quickfix', open = true } },
-      }
-    end,
-    condition = is_meson_project,
+  condition = {
+    callback = function() return vim.fn.filereadable 'meson.build' == 1 end,
   },
-  {
-    name = 'Meson Test',
-    builder = function()
-      return {
-        cmd = { 'meson' },
-        args = { 'test', '-C', 'builddir' },
-        components = { 'default', { 'on_output_quickfix', open = true } },
-      }
-    end,
-    condition = is_meson_project,
-  },
-  {
-    name = 'Meson Clean',
-    builder = function()
-      return {
-        cmd = { 'ninja' },
-        args = { '-C', 'builddir', 'clean' },
-        components = { 'default' },
-      }
-    end,
-    condition = is_meson_project,
-  },
+
+  generator = function(opts, cb)
+    local tasks = {
+      {
+        name = 'Meson: Setup',
+        builder = function() return { cmd = { 'meson', 'setup', 'builddir' }, components = { 'default' } } end,
+      },
+      {
+        name = 'Meson: Compile All',
+        builder = function() return { cmd = { 'meson', 'compile', '-C', 'builddir' }, components = { 'default' } } end,
+      },
+      {
+        name = 'Meson: Test',
+        builder = function() return { cmd = { 'meson', 'test', '-C', 'builddir' }, components = { 'default' } } end,
+      },
+    }
+
+    -- dynamicccc didnt write this btw
+    if vim.fn.isdirectory 'builddir' == 1 then
+      local output = vim.fn.system { 'meson', 'introspect', '--targets', 'builddir' }
+
+      if vim.v.shell_error == 0 and output ~= '' then
+        local ok, parsed_targets = pcall(vim.json.decode, output)
+
+        if ok and type(parsed_targets) == 'table' then
+          for _, target in ipairs(parsed_targets) do
+            if target.name then
+              table.insert(tasks, {
+                name = 'Meson Compile: ' .. target.name,
+                builder = function()
+                  return {
+                    cmd = { 'meson', 'compile', '-C', 'builddir', target.name },
+                    components = { 'default' },
+                  }
+                end,
+              })
+            end
+          end
+        end
+      end
+    end
+
+    -- 3. Return the combined list of static and dynamic tasks to Overseer
+    cb(tasks)
+  end,
 }
